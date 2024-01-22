ffb9fd7543f5dd50968ed3b3a2b48c46ad30a3db
arslan <arslanhafiz091@gmail.com>
2024-01-22 22:30:25 +0500

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,19 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "user_index", to: "users#index"
   get "sign_up", to: "users#new"
   post "sign_up", to: "users#create"
   delete "logout", to: "sessions#destroy"
 
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 
   
 
